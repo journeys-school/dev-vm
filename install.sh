3c58a0dd9ed0854cf56acf3c2d9dccadf97a57fd
Jeremy Hopple <jeremy@jthopple.com>
2016-04-18 08:33:39 -0600

    Add node and postgres

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update -y
-sudo apt-get install -y elixir git
+sudo apt-get install -y elixir git postgresql build-essential
+
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# postgres user
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
