d11e4e040f14e28ceda2a6b6a66a46f7a71dd118
Jeremy Hopple <jeremy@jthopple.com>
2016-04-29 11:24:42 -0600

    Added additional erlang deps

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update -y
-sudo apt-get install -y elixir git postgresql build-essential
+sudo apt-get install -y elixir git postgresql build-essential erlang-dev erlang-parsetools
 
 # install nodejs
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
